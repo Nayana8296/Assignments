e62c986d7821188911d6058cd64a6924bf200d80
Nayana8296 <nayanakenchagundi@gmail.com>
2025-09-06 16:44:22 +0530

    Update CamperTest.java

--- src/test/java/com/TricentisTestCases/CamperTest.java
+++ src/test/java/com/TricentisTestCases/CamperTest.java
@@ -1,19 +1,36 @@
 package com.TricentisTestCases;
 
+import java.time.Duration;
+
 import org.testng.annotations.Test;
 
 import com.TricentisPages.CamperPage;
 
 public class CamperTest extends BaseTest{
   @Test
-  public void fillCamperTestform(){
+  public void fillCamperTestform() throws InterruptedException{
+	  
 	  
 	  CamperPage camperpage = new CamperPage(driver);
+	  driver.get("http://sampleapp.tricentis.com/101/");
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	  driver.manage().window().maximize();
 	  
-	  camperpage.enterVehicledata("Honda","458","12/08/2002","9","yes","Diesel","785","758","4598","dhsabdj","487");
-	  camperpage.enterInsurantdata("kasjw","ajsKD","5/25/2005","Male","hjajs","Aruba","785632",
+	  
+	  camperpage.ClickCamperPage();
+	  camperpage.entervehicledata("Honda","458","12/08/2002","9","yes","Diesel","456","758","4598","dhsabdj","487");
+	  camperpage.ClickNext();
+	  camperpage.enterInsurantdata("kasjw","ajsKD","05/25/2005","male","hjajs","Aruba","785632",
 			                        "haJDJKD","SelfEmployed","Speeding","https://sampleapp.tricentis.com/101/app.php");
-	  camperpage.enterProductdata("3/15/2030","10.000.000,00","Partial Coverage","Legal defence Insurance","yes", null);
+	  camperpage.ClickNext1();
+	  camperpage.enterProductdata("3/15/2030","10.000.000,00","Partial Coverage","EuroProtection");
+	  camperpage.ClickNext5();
+	  camperpage.enterSelectPriceOption("ultimate");
+	  camperpage.ClickNext2();
+	  camperpage.enterSendquote("nandu@gmail.com","8755487785","Nandu","nandu123","nandu123","Good");
+	  camperpage.ClickSend();
+	  camperpage.ClickOk();
+	  camperpage.ClickMainPage();
 	  
   }
 }

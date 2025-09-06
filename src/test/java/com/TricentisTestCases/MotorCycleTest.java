672cf29ac19ed6423c17799df7747ceb924de336
Nayana8296 <nayanakenchagundi@gmail.com>
2025-09-06 16:45:42 +0530

    Update MotorCycleTest.java

--- src/test/java/com/TricentisTestCases/MotorCycleTest.java
+++ src/test/java/com/TricentisTestCases/MotorCycleTest.java
@@ -1,18 +1,38 @@
 package com.TricentisTestCases;
 
+import java.time.Duration;
+
 import org.testng.annotations.Test;
 
 import com.TricentisPages.MotorCyclePage;
 
 public class MotorCycleTest extends BaseTest {
   @Test
-  public void fillMotorCycleformTest() {
-
-            MotorCyclePage motorcycle = new MotorCyclePage(driver);
+  public void fillMotorCycleformTest() throws InterruptedException {
             
-            motorcycle.entervehicledata("Skoda","Scooter","458","885","12/05/2024","2","45680", "785");
+	  driver.get("http://sampleapp.tricentis.com/101/");
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	  
+	  
+	  
+            MotorCyclePage motorcycle = new MotorCyclePage(driver);
+            motorcycle.ClickMotorcycle();
+            
+            motorcycle.entervehicledata("Skoda","Scooter","125","885","12/05/2024","2","45680","785");
+            motorcycle.ClickNext();
+            
             motorcycle.enterInsurantdata("Nsd","sdmd","12/08/1998","female","xckmc","Bahamas","54785","kdfjfv",
-            		                     "Public Official","skydiving","https://sampleapp.tricentis.com/101/app.php");
-            motorcycle.enterProductdata("12/08/2025","5.000.000,00","Full Coverage","Euro Protection");
+            		                     "Public Official","speeding","https://sampleapp.tricentis.com/101/app.php");
+            motorcycle.ClickNext1();     
+            
+            
+            motorcycle.enterProductdata("12/08/2025","5.000.000,00","Full Coverage","Legal Defense Insurance");
+            motorcycle.ClickNext51();
+            motorcycle.enterselectPriceOption("platinum");
+      	    motorcycle.ClickNext2();
+            motorcycle.enterSendquote("Himami@gmail.com","8259657485","Himami","Himami123","Himami123","NA");
+            motorcycle.ClickOk();
+            motorcycle.ClickCamperInsurance();
   }
 }

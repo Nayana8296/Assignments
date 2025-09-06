4e601e0afcff031b8781859d3b6b54a113e1a5ee
Nayana8296 <nayanakenchagundi@gmail.com>
2025-09-06 16:47:45 +0530

    Update TruckTest.java

--- src/test/java/com/TricentisTestCases/TruckTest.java
+++ src/test/java/com/TricentisTestCases/TruckTest.java
@@ -1,9 +1,6 @@
 package com.TricentisTestCases;
-
 import java.time.Duration;
-
 import org.testng.annotations.Test;
-
 import com.TricentisPages.TruckPage;
 
 public class TruckTest extends BaseTest {
@@ -15,13 +12,11 @@ public class TruckTest extends BaseTest {
 	  
 	  TruckPage truckpage = new TruckPage(driver);
 	  truckpage.ClickTruck();
-	 
 	  truckpage.enterVehicledata("BMW","515","08/12/2025","9","Electric Power","125","456","585","486","4586");
 	  truckpage.ClickNext();
 	  truckpage.enterInsurantdata("John","Doe","06/05/2002","Male","Vijayanagar","Australia","78965",
 			                       "Jarkhand","Unemployed","Skydiving","https://sampleapp.tricentis.com/101/app.php");
 	  truckpage.ClickNext1();
-	  
 	  truckpage.enterProductdata("01/12/2026","20.000.000,00","Partial Coverage","Euro Protection");
 	  truckpage.ClickNext5();
 	  truckpage.enterselectPriceOption("gold");
